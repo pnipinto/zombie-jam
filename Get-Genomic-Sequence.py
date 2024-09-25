66451e9662775c9e1ea492649eba96f23574d618
aamantle <aamantle@amazon.com>
2024-09-25 07:18:37 +0000

    feat: Update python scripts to read local file

--- Get-Genomic-Sequence.py
+++ Get-Genomic-Sequence.py
@@ -1,33 +1,14 @@
-import boto3
-import json
 import pandas as pd
 
-client = boto3.client('lambda', region_name='us-east-1')
-
-def invoke_lambda():
-    response = client.invoke(
-        FunctionName='JAM-Quigley-Get-Data',
-        InvocationType='RequestResponse'
-    )
-    
-    # Read the response payload
-    response_payload = response['Payload'].read().decode('utf-8')
-    
-    # Parse the JSON response
-    result = json.loads(response_payload)
-    
-    # Extract the body
-    body = json.loads(result['body'])
-
-    # Convert the JSON list to a DataFrame
-    df = pd.DataFrame(body)
+def read_csv():
+    # Load the data
+    df = pd.read_csv('data/zombie-data.csv')
 
     # Print the DataFrame
-    print(df)
+    print(df.head())
 
 def main():
-    invoke_lambda()
-
+    read_csv()
 
 if __name__ == "__main__":
     main()
